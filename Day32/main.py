f131b102a66131d10264bc352817c7b3f6cd6da2
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-11 09:59:13 +0430

    datetime module in python.

--- Day32/main.py
+++ Day32/main.py
@@ -1,12 +1,23 @@
-import smtplib
+# import smtplib
 
-my_email = "appbreweryinfo@gmail.com"
-password = "wer432"
-connection = smtplib.SMTP("smtp.gmail.com")
+# my_email = "appbreweryinfo@gmail.com"
+# password = "wer432"
+# connection = smtplib.SMTP("smtp.gmail.com")
 # this line will make this connection secure.
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs="appbreweryinfo@gmail.com",
-                    msg="Subject:Hello\n\nThis is the body of my email")
-connection.close()
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(from_addr=my_email,
+#                    to_addrs="appbreweryinfo@gmail.com",
+#                     msg="Subject:Hello\n\nThis is the body of my email")
+# connection.close()
+import datetime as dt
+
+# now method gets us current time and date:
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(year)
+
+date_of_birth = dt.datetime(year=1990, month=12, day=15)
+print(date_of_birth)
